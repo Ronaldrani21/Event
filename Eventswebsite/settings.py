fa55e7f3bd956b887cb172175d46eb782b0bc044
Ampeire Ronald <73988385+Ronaldrani21@users.noreply.github.com>
2021-05-24 19:39:46 +0300

    Update settings.py

--- Eventswebsite/settings.py
+++ Eventswebsite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xaq*iwr&*#zd)ut77i798(q!@)5=l0#!-%=yy^y5b#6m1hx+4j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','conf-website.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
