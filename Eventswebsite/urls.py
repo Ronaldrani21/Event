ca50350af0a70f9e1a93ac5610d09c8d1b835211
Ampeire Ronald <73988385+Ronaldrani21@users.noreply.github.com>
2021-05-24 19:38:54 +0300

    Update urls.py

--- Eventswebsite/urls.py
+++ Eventswebsite/urls.py
@@ -19,14 +19,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from django.views.static import serve
-from django.conf.urls import url
 
 urlpatterns = [
     path('', include('Conference.urls', namespace='Conference')),
     path('admin/', admin.site.urls),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
